c09bfa633bfb50a1d09e83b73c0896f0517eb8c5
Lyubomir <stamenovl99@gmail.com>
2022-01-13 14:25:28 +0200

    implemented gather method

--- src/engine/Engine.ts
+++ src/engine/Engine.ts
@@ -68,38 +68,84 @@ export default class Engine {
         throw new Error(`Unit ${name} does not exist!`);
     }
 
+    private deleteDeadUnits(units: Unit[]): void{
+        units.forEach(u => u.team === Team.BLUE ? this._blueTeam.deleteUnit(u) : this._redTeam.deleteUnit(u));
+    }
+
     public performAttack(attackerName: string): string{
-        const attacker: Unit = this.getUnitByName(attackerName);
-        const enemyTeam: Team = attacker.team === Team.BLUE ? Team.RED : Team.BLUE;
-        const defenders: Unit[] = enemyTeam === Team.BLUE ? 
-        this._blueTeam.getUnitsByPosition(attacker.position)
-         : this._redTeam.getUnitsByPosition(attacker.position);     
-        const allies: Unit[] = enemyTeam === Team.RED ? 
-        this._blueTeam.getUnitsByPosition(attacker.position)
-         : this._redTeam.getUnitsByPosition(attacker.position);
+        try{
+            const attacker: Unit = this.getUnitByName(attackerName);
+            const enemyTeam: Team = attacker.team === Team.BLUE ? Team.RED : Team.BLUE;
+            const defenders: Unit[] = enemyTeam === Team.BLUE ? 
+            this._blueTeam.getUnitsByPosition(attacker.position)
+            : this._redTeam.getUnitsByPosition(attacker.position);     
+            const allies: Unit[] = enemyTeam === Team.RED ? 
+            this._blueTeam.getUnitsByPosition(attacker.position)
+            : this._redTeam.getUnitsByPosition(attacker.position);
 
-        if(defenders.length === 0 && allies.length > 1){
-            return `You cannot attack your friends, dummy!`;
-        }
+            if(defenders.length === 0 && allies.length > 1){
+                return `You cannot attack your friends, dummy!`;
+            }
 
-        if(defenders.length === 0 && allies.length === 1){
-            return `There is nothing to attack!`;
-        }
+            if(defenders.length === 0 && allies.length === 1){
+                return `There is nothing to attack!`;
+            }
 
 
-        if(attacker.type === UnitType.NINJA){
-            let damageTaken: number = this._unitUtils.getUnitsHealhtPoints(defenders);
-            defenders.forEach(d => attacker.attackEnemy(d));
-            damageTaken -= this._unitUtils.getUnitsHealhtPoints(defenders);
-            const deadUnits: Unit[] = this._unitUtils.getDeadUnits(defenders);
-            return `There was a fierce fight between ${attacker.name} - attacker and ${this._unitUtils.getDefendersAsString(defenders)} - defenders. The defenders took totally 0 damage. The attacker took ${damageTaken} damage. There are ${deadUnits.length} dead units after the fight was over - ${this._unitUtils.getDefendersAsString(deadUnits)}`;
-        }else {
-            const defender = this._unitUtils.getRandomUnit(defenders);
-            const attackerHealthBeforeFight = attacker.healthPoints;
-            const defenderHealthBeforeFight = defender.healthPoints;
-            attacker.attackEnemy(defender);
-            const deadUnits: Unit[] = this._unitUtils.getDeadUnits([attacker, defender]);
-            return `There was a fierce fight between ${attacker.name} - attacker and ${defender.name} - defender. The defender took totally ${attackerHealthBeforeFight - attacker.healthPoints}. The attacker took ${defenderHealthBeforeFight - defender.healthPoints} damage. There are ${deadUnits.length} dead units after the fight was over - ${this._unitUtils.getDefendersAsString(deadUnits)}`;
+            if(attacker.type === UnitType.NINJA){
+                let damageTaken: number = this._unitUtils.getUnitsHealhtPoints(defenders);
+                defenders.forEach(d => attacker.attackEnemy(d));
+                damageTaken -= this._unitUtils.getUnitsHealhtPoints(defenders);
+                const deadUnits: Unit[] = this._unitUtils.getDeadUnits(defenders);
+                console.log(defenders);
+                this.deleteDeadUnits(deadUnits);
+                return `There was a fierce fight between ${attacker.name} - attacker and ${this._unitUtils.getDefendersAsString(defenders)} - defenders. The defenders took totally 0 damage. The attacker took ${damageTaken} damage. There are ${deadUnits.length} dead units after the fight was over - ${this._unitUtils.getDefendersAsString(deadUnits)}`;
+            }else {
+                const defender = this._unitUtils.getRandomUnit(defenders);
+                const attackerHealthBeforeFight = attacker.healthPoints;
+                const defenderHealthBeforeFight = defender.healthPoints;
+                attacker.attackEnemy(defender);
+                const deadUnits: Unit[] = this._unitUtils.getDeadUnits([attacker, defender]);
+                console.log(defender);
+                this.deleteDeadUnits(deadUnits);
+                return `There was a fierce fight between ${attacker.name} - attacker and ${defender.name} - defender. The defender took totally ${attackerHealthBeforeFight - attacker.healthPoints}. The attacker took ${defenderHealthBeforeFight - defender.healthPoints} damage. There are ${deadUnits.length} dead units after the fight was over - ${this._unitUtils.getDefendersAsString(deadUnits)}`;
+            }
+        }catch(err){
+           return (<Error>err).message;
         }
     }
+
+    private getResourceByPosition(position: Position): (Resource | undefined){
+        return this._resources.find(res => res.position.x === position.x && res.position.y === position.y);
+    }
+
+    private deleteResource(resource: Resource): void{
+        for(let i = 0; i < this._resources.length; i++){
+            if(resource === this._resources[i]){
+                this._resources.splice(i, 1);
+                return;
+            }
+        }
+    }
+
+    public performGather(unitName: string): string{
+        try{
+            const unit: Unit = this.getUnitByName(unitName);
+            const resource: (Resource | undefined) = this.getResourceByPosition(unit.position);
+            if(!resource){
+                return `There is nothing to gather`;
+            }
+            unit.gatherResource(resource);
+            if(unit.team === Team.BLUE){
+                this._blueTeam.resourceStorage.addResource(resource);
+            }
+            if(unit.team === Team.RED){
+                this._redTeam.resourceStorage.addResource(resource);
+            }
+            this.deleteResource(resource);
+            return `Successfully gathered ${resource.quantity} ${resource.type.toLowerCase()}. Team ${unit.team} now has ${unit.team === Team.BLUE ? this._blueTeam.resourceStorage.getResourcesInformation() : this._redTeam.resourceStorage.getResourcesInformation()}.`;
+        }catch(err){
+           return (<Error>err).message;
+        }
+    } 
 }
