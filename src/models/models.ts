8e0d890a555c909778d8130c44f8da5be3815d67
Anelia Radeva <anelia_radeva@epam.com>
2022-01-20 11:47:47 +0200

    Added methods to models

--- src/models/models.ts
+++ src/models/models.ts
@@ -1,5 +1,6 @@
 import { ResourceType, Team, UnitType } from "src/classes/enum.types";
 import Resource from "src/classes/Resource";
+import Unit from "src/classes/Unit";
 import TeamEntity from "src/engine/TeamEntity";
 
 export interface Position {
@@ -14,11 +15,19 @@ export interface WorldObjectModel {
     canMove: boolean;
     team: Team;
 
+    modifyPosition(position: Position): void;
+
+    modifyHealthPoints(value: number): void;
+
 }
 
 export interface ResourceModel {
     quantity: number;
     type: ResourceType;
+
+    getInformation(): string;
+
+    toString(): string;
 }
 
 export interface UnitModel {
@@ -27,15 +36,49 @@ export interface UnitModel {
     defense: number;
     canGather: boolean;
     type: UnitType;
+
+    attackEnemy(enemy: Unit): void;
+
+    move(enemyPosition: Position): void;
+
+    gatherResource(resource: Resource): void
+
+    getInformation(): string;
 }
 
 export interface ResourceStorageModel {
     lumberStored: Resource;
     foodStored: Resource;
     ironStored: Resource;
+
+    addResource(resource: Resource): void;
+
+    getResourcesInformation(): string;
+
+    getStoragePoints(): number;
 }
 
 export interface EngineModel {
     redTeam: TeamEntity;
     blueTeam: TeamEntity;
+
+    createUnit(name: string, positionAsString: string, teamAsString: string, unitTypeAsString: string): string;
+
+    createResource(type: string, position: string, quantity: string): string;
+
+    performAttack(attackerName: string): string;
+
+    performGather(unitName: string): string;
+
+    performGo(unitName: string, positionAsString: string): string;
+
+    endGame(): string;
+
+    showAll(): string;
+
+    getUnitInformationByCoordinates(positionAsString: string): string;
+
+    showAllResources(): string;
+
+    showAllUnitsByTeam(teamAsString: string): string;
 }
